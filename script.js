598134e8091492e071f64b129f47a288c74f91e7
HI K O <schoheik@googlemail.com>
2023-10-29 22:07:54 +0100

    header überarbeitet

--- script.js
+++ script.js
@@ -8,6 +8,49 @@ $('#audio-control').click(function(){
      $(this).text('Unmute');
    }
 });
+
+
+
+var contactpopup = document.getElementById('contact-popup');
+var contactbutton = document.getElementById('openContact');
+
+contactbutton.onclick = function() {
+  window.alert("xxx");
+  contactpopup.style.display = 'block';
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+      closeModal();
+  }
+}
+
+
+window.addEventListener('load', function() {
+  window.alert("xxx");
+  const hintergrund = document.getElementById('hintergrund');
+  const sticker = document.getElementById('sticker');
+
+  // Funktion zur Skalierung des Stickers relativ zur Größe des Hintergrundbildes
+  function skalieren() {
+      const hintergrundBreite = hintergrund.clientWidth;
+      const stickerBreiteProzent = 5; // Ändern Sie dies nach Bedarf
+      const stickerBreite = (hintergrundBreite * stickerBreiteProzent) / 100;
+
+      sticker.style.width = stickerBreite + 'px';
+  }
+
+  // Initial skalieren und bei Änderungen des Fensters erneut skalieren
+  skalieren();
+  window.addEventListener('resize', skalieren);
+});
+
+
+/* https://www.youtube.com/@skarabaeusband3590 */
+
+
+
+
 /*
 function toggleVisibility() {
   var image = document.getElementById("AlexBild");
@@ -50,13 +93,15 @@ function toggleVisibility() {
 }
 */
 
-var image = document.getElementById("AlexBild");
-var overlay = document.getElementById("overlayAlex")
-overlay.addEventListener('click',function() {
-  if (image.style.opacity === "0") {
-    image.style.opacity = "1";
-  } else {
-    image.style.opacity = "0"
+
+function toggleVisibility() {
+  var image = document.getElementById("AlexBild");
+  window.alert("function1");
+  if (image.style.opacity === 0) {
+    window.altert("fct10");
+    image.style.opacity = 1;
+  } else if (image.style.opacity === 1) {
+    window.altert("fct11");
+    image.style.opacity = 0;
   }
-  }
-)
+}
