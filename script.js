66d6fd1ffbcf0d1dd4b278e468f1d14f457dc102
HI K O <schoheik@googlemail.com>
2023-11-03 20:59:18 +0100

    1st try carousel

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 var contactpopup = document.getElementById('contact-popup');
 var contactbutton = document.getElementById('openContact');
 
+const carousel = document.querySelector('.sm_carousel');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+
+let currentIndex = 0;
+
 contactbutton.onclick = function() {
   contactpopup.style.display = 'block';
 }
@@ -13,4 +19,18 @@ window.onclick = function(event) {
   if (event.target == contactpopup) {
       closeModal();
   }
+}
+
+
+nextButton.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % carousel.children.length;
+  updateCarousel();
+});
+prevButton.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + carousel.children.length) % carousel.children.length;
+  updateCarousel();
+});
+function updateCarousel() {
+  const translateX = currentIndex * -300;
+  carousel.style.transform = `translateX(${translateX}px)`;
 }
