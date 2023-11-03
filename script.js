85909faf2f45886397bd8c8dd608e94651421cc6
HI K O <schoheik@googlemail.com>
2023-11-03 21:46:17 +0100

    fix carousel

--- script.js
+++ script.js
@@ -21,16 +21,35 @@ window.onclick = function(event) {
   }
 }
 
-
 nextButton.addEventListener('click', () => {
   currentIndex = (currentIndex + 1) % carousel.children.length;
   updateCarousel();
 });
+
 prevButton.addEventListener('click', () => {
   currentIndex = (currentIndex - 1 + carousel.children.length) % carousel.children.length;
   updateCarousel();
 });
+
 function updateCarousel() {
-  const translateX = currentIndex * -300;
+  for (let i = 0; i < carousel.children.length; i++) {
+      const item = carousel.children[i];
+      item.classList.remove('selected');
+  }
+  const translateX = currentIndex * -200;
   carousel.style.transform = `translateX(${translateX}px)`;
+
+  /* // Automatisches Zurücksetzen auf das erste Element nach dem letzten Element
+  if (currentIndex === carousel.children.length - 1) {
+      setTimeout(() => {
+          currentIndex = 0;
+          updateCarousel();
+      }, 500); // 500 Millisekunden für die Animation (Anpassen Sie die Zeit nach Bedarf)
+  } */
+  const selectedItem = carousel.children[currentIndex];
+  selectedItem.classList.add('selected');
 }
+
+// Initialisierung des Carousels
+updateCarousel();
+
